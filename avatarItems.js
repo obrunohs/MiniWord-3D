0bd739a9e061838500c1d57c9df3de0f00b213cc
obrunohs <scripthc3@gmail.com>
2026-06-03 04:39:06 -0300

    Add files via upload

---
+++ avatarItems.js
@@ -0,0 +1,6 @@
+export const avatarOptions={
+skins:['#f2b879','#d8a271','#8d5524'],
+hairs:['#020617','#4b2e1f','#ca8a04','#7c2d12'],
+shirts:['#2563eb','#db2777','#16a34a','#7c3aed'],
+pants:['#1e3a8a','#831843','#14532d','#312e81']
+};
